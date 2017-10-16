ddc37af10f4370fd70f4de9fda2acb29046ea127
Stake Zhang <starkey86@163.com>
2017-10-16 23:11:43 +0800

    add Text Changed Listener

--- app/src/main/java/com/example/stake/autoshrinktextview/MainActivity.java
+++ app/src/main/java/com/example/stake/autoshrinktextview/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.stake.autoshrinktextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt;
+    EditText edit;
+    LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.txt);
+        edit = (EditText) findViewById(R.id.edit);
+        layout = (LinearLayout) findViewById(R.id.layout);
+        edit.setText(txt.getText());
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                txt.setText(s);
+                txt.requestLayout();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
